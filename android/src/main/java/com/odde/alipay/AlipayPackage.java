01517e80f871ee933d0d06c4fbe480eef0e792dd
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-01-05 16:37:17 +0800

    android: remove one unnecessary method in AlipayPackage

--- android/src/main/java/com/odde/alipay/AlipayPackage.java
+++ android/src/main/java/com/odde/alipay/AlipayPackage.java
@@ -24,11 +24,6 @@ public class AlipayPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return emptyList();
     }
